855ec698dbc098756b0f4d3c6887b5d1383e0712
IgnatiusMD <michaeldinata33@gmail.com>
2021-10-03 00:23:42 +0700

    first commit

---
+++ src/pages/about-page/aboutPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+    return (
+        <React.Fragment>
+            <div>ABOUT PAGE</div>
+        </React.Fragment>
+    )
+}
+
+export default AboutPage;
---
+++ src/pages/home-page/homePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <React.Fragment>
+            <div className="home-title">
+                HOME PAGE
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default HomePage;
